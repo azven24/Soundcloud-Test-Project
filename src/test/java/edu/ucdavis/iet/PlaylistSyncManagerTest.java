52a114b848913852775f200cb20133395cdc27b1
Arthur Zvenigorodsky <azven@ucdavis.edu>
2014-07-08 13:56:10 -0700

    Cleaned up some code, function passes all tests so far

--- src/test/java/edu/ucdavis/iet/PlaylistSyncManagerTest.java
+++ src/test/java/edu/ucdavis/iet/PlaylistSyncManagerTest.java
@@ -20,17 +20,10 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class PlaylistSyncManagerTest {
-
-	/*@Test
-	public void testParseXMLconvertJSON() {
-		fail("Not yet implemented");
-	} */
-
+public class PlaylistSyncManagerTest
+{
 	// Create an instance of the PlaylistSyncManager class to test
 	PlaylistSyncManager psm = new PlaylistSyncManager();
-	
-  
 	
 	@Test
 	public void testWritetoJSONFile() throws SAXException, IOException, ParserConfigurationException, XPathExpressionException 
@@ -46,10 +39,8 @@ public class PlaylistSyncManagerTest {
 		NodeList trackList = (NodeList) xPath.compile(downloadableTrack).evaluate(xmlDocument, XPathConstants.NODESET);
 		NodeList urlList = (NodeList) xPath.compile(downloadURL).evaluate(xmlDocument, XPathConstants.NODESET);
 		
-		// i made a change here
-		
-		
-		writetoJSONFile(trackList, urlList);
+		// Testing function's arguments
+		psm.writetoJSONFile(trackList, urlList);
 		
 		// Testing valid input: both must be NodeLists
 		assertTrue(trackList instanceof NodeList);
@@ -60,33 +51,6 @@ public class PlaylistSyncManagerTest {
 		assertNotNull(urlList);
 		
 		// Testing invalid input: if one of the URL's is invalid
-		fail("Not yet implemented");
+		
 	}
-	
-	
-	/*public void createFile() throws ParserConfigurationException, SAXException, IOException, XPathExpressionException
-	{
-		FileInputStream xmlFile = new FileInputStream("/Users/azven224/Documents/sc/esb/soundcloudapp/flows/playlistInfo.xml");
-		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = builderFactory.newDocumentBuilder();
-		Document xmlDocument = builder.parse(xmlFile);
-		XPath xPath = XPathFactory.newInstance().newXPath();
-		
-		String downloadableTrack = "/playlist/tracks/track[downloadable = 'true']/title";
-		String downloadURL = "/playlist/tracks/track[downloadable = 'true']/download-url";
-		NodeList trackList = (NodeList) xPath.compile(downloadableTrack).evaluate(xmlDocument, XPathConstants.NODESET);
-		NodeList urlList = (NodeList) xPath.compile(downloadURL).evaluate(xmlDocument, XPathConstants.NODESET);
-		
-	} */
-
-	/*
-	public void createTracks()
-	{
-		NodeList
-	}
-	
-	public void createURLs()
-	{
-		
-	} */
 }
